284d58844a69ebdd9f16c2978c91c4645bcf125e
Taly <vitalik7tv@yandex.ru>
2025-10-26 10:32:46 +0300

    add metadata for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
-import { formatDate, getNotesPosts } from '@/app/notes/utils';
 import ProjectsBlock from './components/ProjectsBlock';
 import NotesBlock from './components/NotesBlock';
+
+export const metadata = {
+  title: 'Виталий Гурын',
+  description: 'Мои проекты и заметки об экспериментах в IT',
+};
 
 export default async function Page() {
   return (
